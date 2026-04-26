fda41a91190198af798c1c7b94712081ae103511
I4mG-r00t <luca.cipriano@protonmail.com>
2026-04-26 13:50:50 -0400

    terraform bootstrap

---
+++ terraform/bootstrap/backend/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "rg-ai-sec-lab-tfstate"
+  location = var.location
+
+  tags = {
+    project = "azure-ai-sec-lab"
+    purpose = "terraform-state"
+  }
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "sttfstate${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    project = "azure-ai-sec-lab"
+    purpose = "terraform-state"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
---
+++ terraform/bootstrap/backend/variables.tf
@@ -0,0 +1,5 @@
+variable "location" {
+  description = "Azure region for the Terraform state backend."
+  type        = string
+  default     = "westeurope"
+}
